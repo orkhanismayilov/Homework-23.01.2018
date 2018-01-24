2f511061deb8d0f18a2dbd5ddd823cf350da75c8
Orkhan Ismayilov <34453008+orkhanismayilov@users.noreply.github.com>
2018-01-25 00:02:06 +0400

    Homework #22 v1.01

--- js/script.js
+++ js/script.js
@@ -1,13 +1,76 @@
-var add = document.querySelectorAll(".add");
-var message = document.querySelector(".input");
+var add = document.querySelector(".add");
+var msg = document.querySelector(".input");
 
-for (i = 0; i < add.length; i++) {
-    add[i].addEventListener("click", function () {
-        var div = document.createElement("DIV");
-        var a = '<input type="text" class="input" placeholder="Type Something"><button class="add-a">+</button>';
+add.addEventListener("click", function (event) {
+    // Function
+    var value = msg.value;
+    var input = document.createElement("INPUT");
+    var btn = document.createElement("BUTTON");
+    var div = document.createElement("DIV");
+
+    if (value != "") {
+        // Input creation
+        input.classList.add("input");
+        input.setAttribute("type", "text");
+        input.setAttribute("value", msg.value);
+
+        // Button creation
+        btn.classList.add("remove");
+        btn.innerText = "-";
+        //Creating function to remove the field
+        btn.addEventListener("click", function () {
+            this.parentNode.parentNode.removeChild(this.parentNode);
+        });
+
+        // Div creation
         div.classList.add("field");
-        div.innerHTML = a;
+        div.appendChild(input);
+        div.appendChild(btn);
+
+        // Append div to container
         document.querySelector(".container").appendChild(div);
-        message.value = "";
-    })
-}
+
+        // Clear input field
+        msg.value = "";
+    } else {
+        alert("Please, enter data");
+    }
+});
+
+msg.addEventListener("keyup", function () {
+    // Function
+    var value = msg.value;
+    var input = document.createElement("INPUT");
+    var btn = document.createElement("BUTTON");
+    var div = document.createElement("DIV");
+
+    if (event.which == 13) {
+        if (value != "") {
+            // Input creation
+            input.classList.add("input");
+            input.setAttribute("type", "text");
+            input.setAttribute("value", msg.value);
+
+            // Button creation
+            btn.classList.add("remove");
+            btn.innerText = "-";
+            //Creating function to remove the field
+            btn.addEventListener("click", function () {
+                this.parentNode.parentNode.removeChild(this.parentNode);
+            });
+
+            // Div creation
+            div.classList.add("field");
+            div.appendChild(input);
+            div.appendChild(btn);
+
+            // Append div to container
+            document.querySelector(".container").appendChild(div);
+
+            // Clear input field
+            msg.value = "";
+        } else {
+            alert("Please, enter data");
+        }
+    }
+});
